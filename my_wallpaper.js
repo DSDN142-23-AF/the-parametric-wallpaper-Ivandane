8b3b8a3d699e828ab329d3dbaf508a193359cf42
Ivandane Mangubat <mangubivan@gmail.com>
2023-11-25 00:00:59 +1300

    Added water background
    Created functions for the water background, deleted stems and leaves

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,151 +1,184 @@
+/*
+NOTE:
+The wallpaper may take a few seconds to load due to the time it requires
+to render the water pattern in the background.
+*/
+
 // PARAMETRIC VARIABLES
+let time = 'day';
+let numRipples = 75;
+
 // Refer to this image for rose patterns: https://en.wikipedia.org/wiki/Rose_(mathematics)#/media/File:Rose-rhodonea-curve-7x9-chart-improved.svg
 // For best results, do not set the same value for 'roseN' and 'roseD'
 let roseN = 7;
 let roseD = 4;
-let roseX = 125; // Takes 0 to 200
-let roseY = 75; // Takes 0 to 200
+let roseX = 100; // Takes 0 to 200
+let roseY = 100; // Takes 0 to 200
 let roseRotate = 0; // Takes 0 to 360
 let roseRadius = 30;
-let roseColorOne = [280, 100, 100]; // H, S, B
-let roseColorTwo = [250, 100, 100]; // H, S, B
+let roseColorOne = [170, 0, 255]; // R, G, B
+let roseColorTwo = [43, 0, 255]; // R, G, B
 
 let pistilRadius = 10;
-let pistilColor = [60, 100, 100]; // H, S, B
-
-let leafLeftScale = 1;
-let leafRightScale = 1;
-let leafLeftRotate = 15;
-let leafRightRotate = 15;
+let pistilColor = [255, 255, 0]; // R, G, B
 
 // SET UP WALLPAPER FUNCTION
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); // Set to false when ready to print
 
-  // Grid Settings
+  // Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
+
+  angleMode(DEGREES); // Set angle mode to degrees
+  pixelDensity(1); // Set pixel density to 1
+  noLoop(); // Turn off loop
 }
 
 // WALLPAPER BACKGROUND FUNCTION
 function wallpaper_background() {
-  colorMode(HSB, 360, 100, 100, 100); // Set colour mode to HSB
-  angleMode(DEGREES); // Set angle mode to degrees
-  background(255); //White
+  // Run water function
+  water();
 }
 
 // DRAW SYMBOL FUNCTION
 function my_symbol() {
-  // DECLARE VARIABLES
-  let stemEndX = 100 - roseX;
-  let stemEndY = 150 - roseY;
-  let stemCurve = -(roseX - 100);
+  polarRose(roseX, roseY, roseRotate);
+}
 
-  // Set new (0, 0) co-ordinate
-  translate(roseX, roseY);
+// WATER FUNCTION
+function water() {
+  // Declare arrays
+  let vectors = [];
+  let distances = [];
 
-  // STEM
+  // Repeat for the number of ripples
+  for(let currentVector = 0; currentVector <= numRipples; currentVector ++) {
+    // Create random vectors in the vectors array
+    vectors[currentVector] = createVector(random(width), random(height));
+  }
+
+  // Load pixels
+  loadPixels();
+
+  // Repeat for all pixels
+  for(let x = 0; x < width; x ++) {
+    for(let y = 0; y < height; y ++) {
+      // Repeat for all values in the vectors array
+      for(let currentDistance = 0; currentDistance < vectors.length; currentDistance ++) {
+        // Calculate the distance between the vectors and add them to the distances array
+        distances[currentDistance] = dist(x, y, vectors[currentDistance].x, vectors[currentDistance].y);
+      }
+
+      // Declare distances sorted array
+      let sorted = sort(distances);
+
+      // Declare noise by the first value in the sorted array
+      let noise = sorted[0];
+
+      // Declare current pixel
+      let currentPixel = (x + y * width) * 4;
+
+      // Set time to day
+      if(time.toLowerCase() == 'day') {
+        // Set color to all RGBA pixels
+        pixels[currentPixel] = waterColor(noise, 14.5, 44, 2.5); // Red
+        pixels[currentPixel + 1] = waterColor(noise, 21, 169, 2.5); // Green
+        pixels[currentPixel + 2] = waterColor(noise, 40, 225, 3); // Blue
+        pixels[currentPixel + 3] = 255; // Alpha
+      }
+
+      // Set time to night
+      if(time.toLowerCase() == 'night') {
+        // Set color to all RGBA pixels
+        pixels[currentPixel] = waterColor(noise, 40, 32, 2.2); // Red
+        pixels[currentPixel + 1] = waterColor(noise, 30, 55, 3.34); // Green
+        pixels[currentPixel + 2] = waterColor(noise, 30, 68, 3.55); // Blue
+        pixels[currentPixel + 3] = 255; // Alpha
+      }
+
+      // Set time to day by default if given an incorrect variable
+      else {
+        // Set color to all RGBA pixels
+        pixels[currentPixel] = waterColor(noise, 14.5, 44, 2.5); // Red
+        pixels[currentPixel + 1] = waterColor(noise, 21, 169, 2.5); // Green
+        pixels[currentPixel + 2] = waterColor(noise, 40, 225, 3); // Blue
+        pixels[currentPixel + 3] = 255; // Alpha
+      }
+    }
+  }
+
+  // Update Pixels
+  updatePixels();
+
   // Start new state
   push();
-  // Set Parameters
-  noFill(); // Set no fill color
-  stroke(120, 100, 100); // Set stroke color (green)
-  strokeWeight(1.5); // Set stroke thickness
-  // Draw Stem
-  bezier(0, 0, 0, 0, stemCurve, 0, stemEndX, stemEndY);
+
+  // Set no stroke
+  noStroke();
+
+  // Draw vertex points
+  beginShape(POINTS);
+  // Repeat for all values in the vectors array
+  for(let currentVertex = 0; currentVertex < vectors.length; currentVertex ++) {
+    // Create a vertex point from the co-ordinates in the vectors array
+    vertex(vectors[currentVertex].x, vectors[currentVertex].y);
+  }
+  endShape();
+
   // Restore previous state
   pop();
+}
 
-  // POLAR ROSE
+// WATER COLOR FUNCTION
+function waterColor(x, division, addition, exponent) {
+  if(x < 0) {
+    return addition;
+  }
+  else {
+    return pow(x / division, exponent) + addition;
+  }
+}
+
+// POLAR ROSE FUNCTION
+function polarRose(x, y, rotation) {
+  // PETALS
   // Start new state
-  push(); 
-  // Set Parameters
-  rotate(roseRotate); // Set rotation
-  radialGradient(0, 0, 0, 0, 0, 45, roseColorOne, roseColorTwo, 25); // Set fill and stroke color
+  push();
+
+  // Set parameters
+  rotate(rotation); // Set rotation
   strokeWeight(0.5); // Set stroke thickness
-  // Draw Rose
+  radialGradient(0, 0, 0, 0, 0, 45, roseColorOne, roseColorTwo, 25); // Set fill and stroke color
+
+  // Draw rose
   beginShape();
-  for(let angle = 0; angle <= 360; angle += 0.1) { // Repeat for every angle
+  // Repeat for every angle
+  for(let angle = 0; angle < 360; angle += 0.1) {
     // Declare variables
     // Equations from: https://en.wikipedia.org/wiki/Rose_(mathematics)
     let dAngle = angle * Math.max(roseN, roseD);
     let r = cos((roseN / roseD) * dAngle);
-    let dx = (roseRadius * r) * cos(dAngle);
-    let dy = (roseRadius * r) * sin(dAngle);
-    // Draw points
+    let dx = (roseRadius * r) * cos(dAngle) + x;
+    let dy = (roseRadius * r) * sin(dAngle) + y;
+
+    // Draw vertex points
     vertex(dx, dy);
   }
   endShape();
   
   // PISTIL
-  // Set Parameters
+  // Set parameters
   fill(pistilColor); // Set fill color
   stroke(strokeColor(pistilColor, 25)); // Set stroke color
   strokeWeight(1); // Set stroke thickness
   // Draw Pistil
-  ellipse(0, 0, pistilRadius);
-  // Restore previous state
-  pop();
+  ellipse(x, y, pistilRadius);
 
-  // LEFT LEAF
-  // Set the new (0, 0) co-ordinate
-  translate(stemEndX, stemEndY + 0.5);
-  // Set Parameters
-  fill(120, 100, 100); // Set fill colour (green)
-  noStroke(); // Set no stroke
-  // Start new state
-  push();
-  // Set scale
-  scale(leafLeftScale);
-  // Set rotation
-  rotate(leafLeftRotate);
-  // Draw Leaf
-  beginShape();
-  curveVertex(0, 0);
-  curveVertex(-10, 2.75);
-  curveVertex(-20, 4);
-  curveVertex(-30, 2.75);
-  curveVertex(-40, 0);
-  curveVertex(-30, -2.75);
-  curveVertex(-20, -4);
-  curveVertex(-10, -2.75);
-  endShape(CLOSE);
-  // CENTER LINE
-  // Set Parameters
-  stroke(strokeColor([120, 100, 100], 50)); // Set stroke color
-  strokeWeight(0.5); // Set stroke thickness
-  // Draw center line
-  curve(0, 5, -10, 0, -30, 0, -40, 5);
-  // Restore previous state
-  pop();
-
-  // RIGHT LEAF
-  // Start new state
-  push();
-  // Set scale
-  scale(leafRightScale);
-  // Set rotation
-  rotate(-leafRightRotate);
-  // Draw Leaf
-  beginShape();
-  curveVertex(0, 0);
-  curveVertex(10, -2.75);
-  curveVertex(20, -4);
-  curveVertex(30, -2.75);
-  curveVertex(40, 0);
-  curveVertex(30, 2.75);
-  curveVertex(20, 4);
-  curveVertex(10, 2.75);
-  endShape(CLOSE);
-  // CENTER LINE
-  // Set Parameters
-  rotate(180); // Reset rotation
-  stroke(strokeColor([120, 100, 100], 50)); // Set stroke color
-  strokeWeight(0.5); // Set stroke thickness
-  curve(0, -5, -10, 0, -30, 0, -40, -5); // Draw center line
   // Restore previous state
   pop();
 }
